18c30a5eee5710f72f07170621cb854b34f5c6d5
devPavelM <MPavelMail@gmail.com>
2019-11-10 15:59:22 +0300

    tech commit

---
+++ src/pages/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import  Hero from '../component/Hero'
+import Banner from '../component/Banner'
+import {Link} from 'react-router-dom'
+import Services from "../component/Services"
+import FeaturedRooms from "../component/FeaturedRooms"
+
+export default function Home() {
+    return <>
+    <Hero>
+        <Banner title='luxurious rooms' subtitle='deluxe rooms starting at $299'>
+           <Link to='/rooms' className='btn-primary' >
+               our rooms
+           </Link>
+        </Banner>
+    </Hero>
+        <Services />
+        <FeaturedRooms />
+    </>
+}
